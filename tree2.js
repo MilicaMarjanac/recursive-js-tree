36ae9f40ec4246b11e34dd80ef5fed946cc01570
MilicaMarjanac <milicamarjanac89@gmail.com>
2021-09-14 01:11:05 +0200

    toggle icon added

--- tree2.js
+++ tree2.js
@@ -31,28 +31,41 @@ const TREE_DATA = {
   }
   
   let treeList = document.getElementById("list");
+  
   function printList(obj) {
+    let wrapper = document.createElement("div");
+    wrapper.classList.add("wrap");
+
     let listItem = document.createElement("li");
+    let ItemIcon = document.createElement("div");
+
+    wrapper.appendChild(ItemIcon);
+    wrapper.appendChild(listItem);
+
     listItem.innerHTML = obj.name;
-    listItem.style.marginLeft = "30px";
+    listItem.style.marginLeft = "10px";
+
     for (let i in obj.children) {
+      if (obj.children) {
+        ItemIcon.classList.add("icon2");
+      }
       listItem.appendChild(printList(obj.children[i]));
     }
-    listItem.addEventListener("click", toggle);
-    return listItem;
-  }
-  treeList.appendChild(printList(TREE_DATA));
 
+    ItemIcon.addEventListener("click", toggle);
+    return wrapper;
+  }
+  
+  treeList.appendChild(printList(TREE_DATA));
   printList(TREE_DATA);
 
-  
-
   function toggle(event) {
-  /*   if (event.target.nodeName !== "HTML") { */
-      if (event.target.classList.value === "hide") {
-        event.target.classList.remove("hide");
-      } else {
-        event.target.classList.add("hide");
-      }
-    //}
+    if (event.target.nextElementSibling.classList.value === "hide") {
+      event.target.nextElementSibling.classList.remove("hide");
+      event.target.classList.add("icon2");
+    } else {
+      event.target.nextElementSibling.classList.add("hide");
+      event.target.classList.remove("icon2");
+      event.target.classList.add("icon1");
+    }
   }
