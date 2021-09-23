8884d47b79162e43e4ba5f7ab080d55536362416
MilicaMarjanac <milicamarjanac89@gmail.com>
2021-09-23 20:00:02 +0200

    update

--- tree.js
+++ tree.js
@@ -59,18 +59,8 @@ const TREE_DATA = {
   treeList.appendChild(printList(TREE_DATA));
   printList(TREE_DATA);
 
+
   
-/*   function iterate(list,forceshow) {
-    for (let child of list.children) {
-    //if (child==="Green" or forceshow==true )
-    //if (child.children)
-      console.log(child.children[1])
-      iterate(child.children[1],true)
-      
-    }
-  }
-  iterate(treeList);
-   */
   function toggle(event) {
     if (event.target.nextElementSibling.classList.value === "hide") {
       event.target.nextElementSibling.classList.remove("hide");
@@ -91,17 +81,34 @@ function search() {
   iterate(treeList, entry /* ,forceShow */  );
 }
 
-function iterate(obj, entry, forceShow) {
+
+
+//ne radi ovo al cisto da vidis kako sam razmisljala
+function iterate(obj, entry, forceshow) {
   for (let child of obj.children) {
-    if (
-      child.children[1].innerText.toUpperCase().indexOf(entry) > -1
-      || forceShow === true) {
-      console.log(child)
-      iterate(child.children[1], entry, true);
-    }
-   /*  else {
-      console.log(child)
-      iterate(child.children[1], entry);
-    }*/
+      console.log('A', child)
+      if (
+          child.children[1].innerText.toUpperCase().indexOf(entry) > -1 || forceshow === true) {
+          console.log('B', child)
+          lastChild = child.children[1].children
+          for (let c of lastChild) {
+              //console.log('lastchild',c.children[1].children.length)
+              last = c.children[1].children.length
+              if (last == 0) {
+                  console.log('C', child)
+                  iterate(child.children[1], entry, true)
+              }
+          }
+          iterate(child.children[1], entry)
+      } else {
+          if (child.children[1].children.length == 0) {
+              console.log('Ca', child)
+              child.style.display = ' '
+          }
+          else {
+              child.style.display = 'none'
+              console.log('E', child)
+          }
+      }
   }
-} 
+}
